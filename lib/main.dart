8a451fbb82da67c5ee39e73e281ff253076f6565
khanhpl <khanh.phanluu01@gmail.com>
2021-10-10 01:14:37 +0700

    update homescreen 10-10-21

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
-import 'screens/home/homeScrean.dart';
+import 'screens/home/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Pharmacity layout demo homepage'),
+      home: MyHomePage(),
     );
   }
 }
---
+++ lib/screens/home/up_nav_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+AppBar UpAppBar(context) {
+  return AppBar(
+    backgroundColor: Colors.blue[700],
+    title: Container(
+      height: 40,
+      margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+      padding: EdgeInsets.fromLTRB(0, 5.0, 0, 5.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            color: Colors.grey[400],
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Tìm Trên Pharmacity',
+                  hintStyle: TextStyle(
+                    color: Colors.black26,
+                  )),
+            ),
+          ),
+        ],
+      ),
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: Icon(Icons.notifications_active_outlined),
+        onPressed: () {},
+      ),
+      IconButton(
+        icon: Icon(Icons.shopping_cart_outlined),
+        onPressed: () {},
+      ),
+    ],
+  );
+}
